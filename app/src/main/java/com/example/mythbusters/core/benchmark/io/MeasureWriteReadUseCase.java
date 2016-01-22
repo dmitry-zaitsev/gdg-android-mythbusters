950e6c8f95c4f9f6fb0ee52fca4778626c5617f7
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2016-01-23 00:27:16 +0100

    Implemented MeasureWriteReadUseCaseTest.

--- app/src/main/java/com/example/mythbusters/core/benchmark/io/MeasureWriteReadUseCase.java
+++ app/src/main/java/com/example/mythbusters/core/benchmark/io/MeasureWriteReadUseCase.java
@@ -3,6 +3,8 @@ package com.example.mythbusters.core.benchmark.io;
 import com.example.mythbusters.core.benchmark.Benchmark;
 import com.example.mythbusters.domain.measurement.MeasurementResult;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
@@ -11,6 +13,15 @@ import rx.Observable;
  * Measures performace of write and read operations into storage.
  */
 public class MeasureWriteReadUseCase {
+
+    private static final int[] ITERATIONS = {
+            1,
+            10,
+            100,
+            1000
+    };
+
+    private static final String KEY_NAME = "key";
 
     private final Benchmark benchmark;
     private final Storage storage;
@@ -28,7 +39,63 @@ public class MeasureWriteReadUseCase {
      * @return {@link Observable} which produces single result of measurement.
      */
     public Observable<Result> measure() {
-        throw new UnsupportedOperationException();
+        return Observable.create(subscriber -> {
+            Result result = performMeasurement();
+
+            if (!subscriber.isUnsubscribed()) {
+                subscriber.onNext(result);
+            }
+        });
+    }
+
+    private Result performMeasurement() {
+        List<MeasurementResult> writeResult = measureWriting();
+        List<MeasurementResult> readResult = measureReading();
+
+        return new Result(writeResult, readResult);
+    }
+
+    private List<MeasurementResult> measureWriting() {
+        final ArrayList<MeasurementResult> results = new ArrayList<>();
+
+        final String value = valueProvider.provide();
+
+        for (int iterations : ITERATIONS) {
+            results.add(
+                    runBenchmarkForWriting(value, iterations)
+            );
+        }
+        return results;
+    }
+
+    private MeasurementResult runBenchmarkForWriting(String value, int iterations) {
+        final long time = benchmark.measureOperation(
+                new WriteOperation(value),
+                iterations
+        );
+
+        return new MeasurementResult(iterations, time);
+    }
+
+    private List<MeasurementResult> measureReading() {
+        final ArrayList<MeasurementResult> results = new ArrayList<>();
+
+        for (int iterations : ITERATIONS) {
+            results.add(
+                    runBenchmarkForReading(iterations)
+            );
+        }
+
+        return results;
+    }
+
+    private MeasurementResult runBenchmarkForReading(int iterations) {
+        final long time = benchmark.measureOperation(
+                new ReadOperation(),
+                iterations
+        );
+
+        return new MeasurementResult(iterations, time);
     }
 
     /**
@@ -81,4 +148,41 @@ public class MeasureWriteReadUseCase {
         }
     }
 
+    private class WriteOperation implements Runnable {
+
+        private final String value;
+
+        private int index = 0;
+
+        private WriteOperation(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public void run() {
+            try {
+                storage.write(KEY_NAME + index, value);
+                index++;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    private class ReadOperation implements Runnable {
+
+        private int index = 0;
+
+        @Override
+        public void run() {
+            try {
+                storage.read(KEY_NAME + index);
+                index++;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
 }
